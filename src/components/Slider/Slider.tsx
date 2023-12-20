05b2c195cba5dfc576419b5907eb330bdc0d35ae
KotlovAlex <103385044+KotlovAlex@users.noreply.github.com>
2023-12-20 21:20:16 +0700

    add slider animation

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -4,13 +4,27 @@ import SliderImg1 from '../../images/slider1.svg'
 import SliderImg2 from '../../images/slider2.svg'
 import SliderImg3 from '../../images/slider3.svg'
 
+type additionalClass = {
+  [key: number]: string;
+}
+
 const Slider = () => {
   const sliderImages = [SliderImg1, SliderImg2, SliderImg3];
   const [currentSlider, setCurrentSlider] = useState(1)
   const activeSlider = `${styles.button} ${styles.active}`;
+  const additionalClass: additionalClass = {
+    0: '',
+    1: styles.second,
+    2: styles.third
+  }
+  const imagesClass = `${styles.images} + ${additionalClass[currentSlider]}`
   return (
     <div className={styles.slider}>
-      <img src={sliderImages[currentSlider]} alt='Slider' />
+      <div className={imagesClass}>
+        <img src={sliderImages[0]} alt='Slider' />
+        <img src={sliderImages[1]} alt='Slider' />
+        <img src={sliderImages[2]} alt='Slider' />
+      </div>
       <div className={styles.buttons}>
         <div onClick={() => setCurrentSlider(0)} className={currentSlider === 0 ? activeSlider : styles.button} />
         <div onClick={() => setCurrentSlider(1)} className={currentSlider === 1 ? activeSlider : styles.button} />
